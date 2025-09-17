4b4cd4660b2cd3539fcf43e6abcb6e7f0b09abf2
RugeFX <zackfxg@gmail.com>
2025-09-18 00:34:24 +0700

    feat(libs): added and configured routing, state management, linting, query, and component libraries

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [react(), tailwindcss(), tanstackRouter()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+});
