75a790463e9b310240d6bd850d2dc67b5d98ebb5
muriz-dev <mhmdrizki.dev@gmail.com>
2025-09-21 16:55:14 +0700

    feat(docs): implement scalar ui with openapi for api contract

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,22 @@
 import "dotenv/config";
 import express from "express";
+import scalarDocsRouter from './modules/scalar';
 
 import { db } from "./db";
 import { env } from "./env";
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const data = await db.query.usersTable.findMany();
+// app.get("/", async (req, res) => {
+//   const data = await db.query.usersTable.findMany();
 
-  return res.json({
-    message: "HoHo",
-    data,
-  });
-});
+//   return res.json({
+//     message: "HoHo",
+//     data,
+//   });
+// });
+
+app.use("/docs", scalarDocsRouter);
 
 app.listen(env.APP_PORT, () => {
   console.log(`Server started on port ${env.APP_PORT}`);
