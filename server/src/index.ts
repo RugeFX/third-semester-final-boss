0d5db18cfe2036359e540969897b5473e4bada10
muriz-dev <mhmdrizki.dev@gmail.com>
2025-09-23 21:11:28 +0700

    fix(endpoint); lil typo in import name

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ import parkingLevelRouter from './modules/parking_levels/parking-level.route';
 import transactionRouter from './modules/transactions/transaction.route';
 import userRouter from './modules/users/user.route';
 import memberRouter from './modules/members/member.route';
-import auditLogs from './modules/audit_logs/audit-log.route';
+import auditLogsRouter from './modules/audit_logs/audit-log.route';
 
 import { db } from "./db";
 import { env } from "./env";
@@ -32,7 +32,7 @@ app.use("/parking-levels", parkingLevelRouter);
 app.use("/transactions", transactionRouter);
 app.use("/users", userRouter);
 app.use("/members", memberRouter);
-app.use("/audit-logs", auditLogs);
+app.use("/audit-logs", auditLogsRouter);
 
 app.listen(env.APP_PORT, () => {
   console.log(`Server started on port ${env.APP_PORT}`);
