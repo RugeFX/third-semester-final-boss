95190eb7dcd1856381ea31ba3ab9e3eeebf3e7e0
muriz-dev <mhmdrizki.dev@gmail.com>
2025-09-28 14:39:27 +0700

    fix(docs): error unknown url scheme

--- server/src/modules/scalar/scalar.route.ts
+++ server/src/modules/scalar/scalar.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { apiReference } from '@scalar/express-api-reference';
 import path from 'path';
+import fs from 'fs';
 
 const router = Router();
 
 const specFilePath = path.join(process.cwd(), 'openapi.json');
-const apiSpecification = await import(specFilePath);
+const apiSpecification = JSON.parse(fs.readFileSync(specFilePath, 'utf-8'));
 
 router.use('/', apiReference({
     content: apiSpecification
