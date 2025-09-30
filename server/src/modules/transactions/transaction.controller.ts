940249d0262b0442249613615626ff83d6f3850a
muriz-dev <mhmdrizki.dev@gmail.com>
2025-09-30 09:29:48 +0700

    feat(transaction): mock version of transaction endpoint

--- server/src/modules/transactions/transaction.controller.ts
+++ server/src/modules/transactions/transaction.controller.ts
@@ -1,165 +1,63 @@
 import { Request, Response } from "express";
+import transactionService from "./transaction.service";
+import vehicleDetailService from "../vehicle_details/vehicle-detail.service";
+import { paramsSchema, entryTransactionSchema } from "./transaction.schema";
+import HttpError from "../common/exceptions/http.error";
 
 // Get all transactions
 export const getAllTransactions = async (req: Request, res: Response) => {
-    const transactions = [
-        {
-            id: 1001,
-            status: "EXIT",
-            paid_amount: 20000,
-            access_code: "PAC30223",
-            user_id: 1,
-            user: {
-                id: 1,
-                name: "John Doe",
-                role: "member"
-            },
-            vehicle_detail_id: 501,
-            vehicle_detail: {
-                id: 501,
-                plate_number: "B1234XYZ",
-                category_id: 2,
-                category: {
-                    id: 2,
-                    name: "Motorcycle",
-                    weight: 100
-                }
-            },
-            parking_level_id: 1,
-            parking_level: {
-                id: 1,
-                name: "L1",
-                max_weight: 2500
-            },
-            created_at: "2025-09-23T13:22:08.322Z",
-            updated_at: "2025-09-23T13:22:08.322Z"
-        },
-        {
-            id: 1002,
-            status: "ENTRY",
-            paid_amount: null,
-            access_code: "PAC30243",
-            user_id: 2,
-            user: {
-                id: 2,
-                name: "Jane Doe",
-                role: "member"
-            },
-            vehicle_detail_id: 501,
-            vehicle_detail: {
-                id: 501,
-                plate_number: "A2423XYZ",
-                category_id: 2,
-                category: {
-                    id: 2,
-                    name: "Car",
-                    weight: 200
-                }
-            },
-            parking_level_id: 1,
-            parking_level: {
-                id: 1,
-                name: "L1",
-                max_weight: 2500
-            },
-            created_at: "2025-09-23T13:22:08.322Z",
-            updated_at: "2025-09-23T13:22:08.322Z"
-        }
-    ];
+    const transactions = await transactionService.getAllTransactions();
 
-    res.json({
+    res.status(200).json({
         success: true,
-        code: 200,
         message: "Transactions fetched successfully",
         data: transactions
     });
 };
 
-// Get transaction by ID
-export const getTransactionById = async (req: Request, res: Response) => {
-    const transaction = {
-        id: 1001,
-        status: "EXIT",
-        paid_amount: 20000,
-        access_code: "PAC30223",
-        user_id: 1,
-        user: {
-            id: 1,
-            name: "John Doe",
-            role: "member"
-        },
-        vehicle_detail_id: 501,
-        vehicle_detail: {
-            id: 501,
-            plate_number: "B1234XYZ",
-            category_id: 2,
-            category: {
-                id: 2,
-                name: "Motorcycle",
-                weight: 100
-            }
-        },
-        parking_level_id: 1,
-        parking_level: {
-            id: 1,
-            name: "L1",
-            max_weight: 2500
-        },
-        created_at: "2025-09-23T13:22:08.322Z",
-        updated_at: "2025-09-23T13:22:08.322Z"
-    };
+export const getTransactionByAccessCode = async (req: Request, res: Response) => {
+    const { accessCode } = paramsSchema.parse(req.params);
 
-    res.json({
+    const transaction = await transactionService.getTransactionByAccessCode(accessCode);
+
+    res.status(200).json({
         success: true,
-        code: 200,
         message: "Transaction fetched successfully",
         data: transaction
     });
-};
-
-// Create a new transaction
-export const createTransaction = async (req: Request, res: Response) => {
-    const { status, paid_amount, access_code, user_id, vehicle_detail_id, parking_level_id } = req.body;
-
-    res.json({
-        success: true,
-        code: 200,
-        message: "Transaction created successfully",
-        data: {
-            status,
-            paid_amount,
-            access_code,
-            user_id,
-            vehicle_detail_id,
-            parking_level_id
-        }
-    });
-};
-
-// Update a transaction
-export const updateTransaction = async (req: Request, res: Response) => {
-    const { status, paid_amount, access_code, user_id, vehicle_detail_id, parking_level_id } = req.body;
-
-    res.json({
-        success: true,
-        code: 200,
-        message: "Transaction updated successfully",
-        data: {
-            status,
-            paid_amount,
-            access_code,
-            user_id,
-            vehicle_detail_id,
-            parking_level_id
-        }
-    });
 }
 
-// Delete a transaction
-export const deleteTransaction = async (req: Request, res: Response) => {
-    res.json({
+export const createTransactionEntry = async (req: Request, res: Response) => {
+    const 
+        status = "ENTRY" as const, 
+        paidAmount = 0, 
+        accessCode = Math.floor(Math.random() * (100000 - 999999 + 1)) + 999999;
+    
+    const { userId, parkingLevelId, categoryId, plateNumber } = entryTransactionSchema.parse(req.body);
+
+    // Create vehicle detail first and get its ID
+    const newVehicleDetail = await vehicleDetailService.createVehicleDetail(plateNumber, categoryId); 
+
+    if (!newVehicleDetail) throw new HttpError(500, "Failed to create vehicle detail");
+
+    const newTransaction = await transactionService.createTransaction(status, paidAmount, accessCode.toString(), userId, newVehicleDetail.id, parkingLevelId);
+
+    res.status(200).json({
         success: true,
-        code: 200,
-        message: "Transaction deleted successfully"
+        message: "Transaction created successfully",
+        data: newTransaction
     });
 };
+
+export const updateTransactionExit = async (req: Request, res: Response) => {
+    const { accessCode } = paramsSchema.parse(req.params);
+    const paidAmount = 2000;
+
+    const updatedTransaction = await transactionService.updateTransactionToExit(accessCode, paidAmount);
+
+    res.status(200).json({
+        success: true,
+        message: "Transaction updated successfully",
+        data: updatedTransaction
+    });
+}
---
+++ server/src/modules/transactions/transaction.service.ts
@@ -0,0 +1,96 @@
+import { db } from "../../db";
+import { eq } from "drizzle-orm";
+import { Status, transactionsTable } from "../../db/schema";
+import HttpError from "../common/exceptions/http.error";
+
+// Get all transactions
+export const getAllTransactions = async () => {
+    const transactions = await db.query.transactionsTable.findMany({
+        with: {
+            user: true,
+            vehicleDetail: true,
+            parkingLevel: true,
+        },
+    });
+
+    return transactions;
+};
+
+// Get transaction by ID
+export const getTransactionById = async (transactionId: number) => {
+    const transaction = await db.query.transactionsTable.findFirst({
+        where: eq(transactionsTable.id, transactionId),
+    });
+
+    if (!transaction) throw new HttpError(404, "Transaction not found");
+
+    return transaction;
+};
+
+// Get transaction by Access Code
+export const getTransactionByAccessCode = async (accessCode: string) => {
+    const transaction = await db.query.transactionsTable.findFirst({
+        where: eq(transactionsTable.access_code, accessCode),
+    });
+
+    if (!transaction) throw new HttpError(404, "Transaction not found");
+
+    return transaction;
+};
+
+// Create a new transaction
+export const createTransaction = async (status: Status, paid_amount: number, access_code: string, user_id: number, vehicle_detail_id: number, parking_level_id: number) => {
+    const [ newTransaction ] = await db.insert(transactionsTable).values({
+        status,
+        paid_amount: paid_amount.toString(),
+        access_code,
+        user_id,
+        vehicle_detail_id,
+        parking_level_id,
+        created_at: new Date(),
+        updated_at: new Date(),
+    }).returning();
+
+    return newTransaction;
+};
+
+// Update a transaction
+export const updateTransaction = async (transactionId: number, status: Status, paid_amount: number, access_code: string, user_id: number, vehicle_detail_id: number, parking_level_id: number) => {
+    await getTransactionById(transactionId);
+
+    const [ updatedTransaction ] = await db.update(transactionsTable).set({
+        status,
+        paid_amount: paid_amount.toString(),
+        access_code,
+        user_id,
+        vehicle_detail_id,
+        parking_level_id,
+        updated_at: new Date(),
+    }).where(eq(transactionsTable.id, transactionId)).returning();
+
+    return updatedTransaction;
+};
+
+// Update a transaction to EXIT
+export const updateTransactionToExit = async (access_code: string, paid_amount: number) => {
+    await getTransactionByAccessCode(access_code);
+
+    const [ updatedTransactionExit ] = await db.update(transactionsTable).set({
+        status: "EXIT",
+        paid_amount: paid_amount.toString(),
+        updated_at: new Date(),
+    }).where(eq(transactionsTable.access_code, access_code)).returning();
+    
+    return updatedTransactionExit;
+};
+
+// Delete a transaction
+export const deleteTransaction = async (transactionId: number) => {
+    await getTransactionById(transactionId);
+
+    const deletedTransaction = await db.delete(transactionsTable).where(eq(transactionsTable.id, transactionId));
+
+    return deletedTransaction;
+};
+
+export default { getAllTransactions, getTransactionById, getTransactionByAccessCode, createTransaction, updateTransaction, updateTransactionToExit, deleteTransaction };
